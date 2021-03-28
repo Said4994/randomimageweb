df99583116f135f4d0574b96f258f751c1f6b20d
Said Office Pc <=>
2021-03-28 15:50:00 +0300

    new

--- lib/api.dart
+++ lib/api.dart
@@ -2,10 +2,10 @@ import 'package:dio/dio.dart';
 import 'package:web_404/main.dart';
 import 'package:web_404/model.dart';
 
-Future<List<Resim>> getapi() async {
+Future<List<Resim>> getapi(String value) async {
   try {
     Response response = await Dio().get(
-        "https://api.unsplash.com/photos/random?client_id=$apikey2&count=45");
+        "https://api.unsplash.com/photos/random/?client_id=$apikey2&count=20&query=$value");
 
     return (response.data as List).map((x) => Resim.fromJson(x)).toList();
   } catch (e) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:web_404/api.dart';
 import 'package:web_404/model.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -25,6 +29,16 @@ class Anasayfa extends StatefulWidget {
   _AnasayfaState createState() => _AnasayfaState();
 }
 
+List<String> catagory = [
+  "Color",
+  "Nature",
+  "People",
+  "Architecture",
+  "Technology",
+  "Travel"
+];
+
+String newc = "Random";
 String genericapikey = "ojy9rVHXrIkZGxOSVOlIAF-D4iu4jwPlNJ9TccEFnpM";
 String apikey1 = "ojy9rVHXrIkZGxOSVOlIAF-D4iu4jwPlNJ9TccEFnpM";
 String apikey2 = 'oRC95d5jq5hqf9bsJ3vrl7SKmiagEBcbEwzDDhF9qBQ';
@@ -32,46 +46,87 @@ String apikey3 = 'JnUAcB_5dwbuYD4HHsrHOqy_xbxhm8Z7NNaXfze3KeM';
 
 class _AnasayfaState extends State<Anasayfa> {
   @override
+  @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Stack(
       children: [
         Scaffold(
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.miniEndFloat,
-          floatingActionButton: FloatingActionButton(
-            elevation: 6,
-            tooltip: 'Refresh',
-            onPressed: () {
-              setState(() {
-                buildFutureBuilderImagebody();
-              });
-            },
-            backgroundColor: Colors.white,
-            child: Icon(
-              Icons.refresh_rounded,
-              color: Colors.black,
-            ),
-          ),
           backgroundColor: Colors.black,
           body: buildFutureBuilderImagebody(),
         ),
         Positioned(
-            bottom: 15,
-            left: 100,
-            child: Text(
-              "Created\n by Said KURT",
-              style: GoogleFonts.adventPro(
-                  decoration: TextDecoration.none,
-                  fontSize: 17,
-                  color: Colors.white),
-            ))
+          bottom: 15,
+          left: 100,
+          child: Text(
+            "Created\n by Said KURT",
+            style: GoogleFonts.adventPro(
+                decoration: TextDecoration.none,
+                fontSize: 17,
+                color: Colors.white),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                        spreadRadius: 10,
+                        color: Colors.white,
+                        blurRadius: 10.0,
+                        offset: Offset(0.0, 0.3))
+                  ]),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: catagory.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      SizedBox(
+                        width: width >= 700 ? 120 : 40,
+                      ),
+                      Material(
+                        child: InkWell(
+                          highlightColor: Colors.white,
+                          focusColor: Colors.white,
+                          hoverColor: Colors.white,
+                          splashColor: Colors.white,
+                          onTap: () {
+                            setState(() {
+                              newc = catagory[index];
+                            });
+                          },
+                          child: Text(
+                            catagory[index],
+                            style: GoogleFonts.montserrat(
+                                decoration: TextDecoration.none,
+                                fontSize:
+                                    width >= 700 || height <= 100 ? 20 : 10,
+                                color: Colors.black.withOpacity(0.9)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              height: height / 20,
+              width: width / 1.5,
+            ),
+          ),
+        ),
       ],
     );
   }
 
   FutureBuilder<List<Resim>> buildFutureBuilderImagebody() {
     return FutureBuilder(
-      future: getapi(),
+      future: getapi(newc),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<Resim> resim = snapshot.data;
